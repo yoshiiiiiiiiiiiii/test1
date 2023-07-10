11be7a6862aac3280d444660f72e1bc6e503f1ee
yoshiiiiiiiiiiiii <115221820+yoshiiiiiiiiiiiii@users.noreply.github.com>
2023-07-10 20:35:47 +0930

    Add files via upload

---
+++ sql.php
@@ -0,0 +1,19 @@
+<?php
+
+$dsn = 'mysql:host=localhost;dbname=directory_app;charset=utf8';
+
+$user = 'yoshi_test';
+$pass = 'Yossy325';
+$dbh = new PDO ($dsn,$user,$pass);
+
+$sql = 'SELECT * FROM Directory';
+
+$stmt = $dbh->query($sql);
+$result = $stmt->fetchall(PDO::FETCH_ASSOC);
+var_dump($result);
+$dbh = null;
+
+
+
+?>
+
